15e8367da8b7908d52288ffe13e48c6f06188878
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2015-02-20 15:28:05 -0600

    pt-88511864: bdd for scopes validation when client is not found.

--- src/test/java/integration/AuthRequestFactory/ScopesValidation/InformClientStory.java
+++ src/test/java/integration/AuthRequestFactory/ScopesValidation/InformClientStory.java
@@ -1,6 +1,6 @@
-package integration.AuthRequestFactory;
+package integration.AuthRequestFactory.ScopesValidation;
 
-import integration.AuthRequestFactory.steps.ScopesValidationSteps;
+import integration.AuthRequestFactory.steps.ScopesValidation.InformClientSteps;
 import integration.BaseStory;
 import org.rootservices.authorization.codegrant.factory.AuthRequestFactory;
 import org.rootservices.authorization.persistence.repository.ClientRepository;
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 /**
  * Created by tommackenzie on 2/13/15.
  */
-public class ScopesValidationStory extends BaseStory<ScopesValidationSteps> {
+public class InformClientStory extends BaseStory<InformClientSteps> {
 
     @Autowired
     private AuthRequestFactory authRequestFactory;
@@ -18,8 +18,8 @@ public class ScopesValidationStory extends BaseStory<ScopesValidationSteps> {
     private ClientRepository clientRepository;
 
     @Override
-    public ScopesValidationSteps makeSteps() {
-        return new ScopesValidationSteps(authRequestFactory, clientRepository);
+    public InformClientSteps makeSteps() {
+        return new InformClientSteps(authRequestFactory, clientRepository);
     }
 
 }
--- src/test/java/integration/AuthRequestFactory/steps/ScopesValidation/InformClientSteps.java
+++ src/test/java/integration/AuthRequestFactory/steps/ScopesValidation/InformClientSteps.java
@@ -1,5 +1,6 @@
-package integration.AuthRequestFactory.steps;
+package integration.AuthRequestFactory.steps.ScopesValidation;
 
+import integration.AuthRequestFactory.steps.ExceptionSteps;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
@@ -27,7 +28,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
 /**
  * Created by tommackenzie on 2/13/15.
  */
-public class ScopesValidationSteps extends ExceptionSteps {
+public class InformClientSteps extends CommonSteps {
 
     private AuthRequestFactory authRequestFactory;
     private ClientRepository clientRepository;
@@ -37,30 +38,10 @@ public class ScopesValidationSteps extends ExceptionSteps {
 
     private List<String> clientIds;
     private List<String> responseTypes;
-    private List<String> scopes;
 
-    public ScopesValidationSteps(AuthRequestFactory authRequestFactory, ClientRepository clientRepository) {
+    public InformClientSteps(AuthRequestFactory authRequestFactory, ClientRepository clientRepository) {
         this.authRequestFactory = authRequestFactory;
         this.clientRepository = clientRepository;
-    }
-
-    @Given("the parameter scopes has two items assigned to PROFILE")
-    public void theParameterScopesHasTwoItemsAssignedToProfile() {
-        this.scopes = new ArrayList<>();
-        this.scopes.add("PROFILE");
-        this.scopes.add("PROFILE");
-    }
-
-    @Given("the parameter scopes has one item assigned to UNKNOWN_SCOPE")
-    public void theParameterScopesHasOneItemAssignedToUnkownScope() {
-        this.scopes = new ArrayList<>();
-        this.scopes.add("UnknownScope");
-    }
-
-    @Given("the parameter scopes has one item assigned to a empty string")
-    public void theParameterScopesHasOneItemAssignedToAEmptyString() {
-        this.scopes = new ArrayList<>();
-        this.scopes.add("");
     }
 
     @Given("a client exists in the database, c")
