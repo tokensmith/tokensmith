080ce3b1007ca84512281251c47c8e1491e63003
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2015-06-06 18:26:53 -0500

    pt-96353656: raise AuthorizationCodeNotFound

--- src/main/java/org/rootservices/authorization/grant/code/protocol/token/RequestToken.java
+++ src/main/java/org/rootservices/authorization/grant/code/protocol/token/RequestToken.java
@@ -2,10 +2,11 @@ package org.rootservices.authorization.grant.code.protocol.token;
 
 import org.rootservices.authorization.authenticate.exception.UnauthorizedException;
 import org.rootservices.authorization.exception.BaseInformException;
+import org.rootservices.authorization.grant.code.protocol.token.exception.AuthorizationCodeNotFound;
 
 /**
  * Created by tommackenzie on 5/24/15.
  */
 public interface RequestToken {
-    TokenResponse run(TokenRequest tokenRequest) throws UnauthorizedException, BaseInformException;
+    TokenResponse run(TokenRequest tokenRequest) throws UnauthorizedException, AuthorizationCodeNotFound;
 }
