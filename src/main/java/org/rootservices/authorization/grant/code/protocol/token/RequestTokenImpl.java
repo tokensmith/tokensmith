e01515b46c6c22ffdb074fb9d14a2134a4f00a2c
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2015-07-10 18:38:49 -0500

    pt-96390786: invalid_grant when redirect uri & access reqeust is not found.

--- src/main/java/org/rootservices/authorization/grant/code/protocol/token/RequestTokenImpl.java
+++ src/main/java/org/rootservices/authorization/grant/code/protocol/token/RequestTokenImpl.java
@@ -81,11 +81,15 @@ public class RequestTokenImpl implements RequestToken {
         try {
             authCode = authCodeRepository.getByClientUUIDAndAuthCode(clientUUID, hashedCode);
         } catch (RecordNotFoundException e) {
-            throw new AuthorizationCodeNotFound("Access Request was not found", e, ErrorCode.ACCESS_REQUEST_NOT_FOUND.getCode());
+            throw new AuthorizationCodeNotFound(
+                "Access Request was not found", "invalid_grant", e, ErrorCode.ACCESS_REQUEST_NOT_FOUND.getCode()
+            );
         }
 
         if ( ! doRedirectUrisMatch(tokenRequest.getRedirectUri(), authCode.getAccessRequest().getRedirectURI()) ) {
-            throw new AuthorizationCodeNotFound("Access Request was not found", ErrorCode.REDIRECT_URI_MISMATCH.getCode());
+            throw new AuthorizationCodeNotFound(
+                "Access Request was not found", "invalid_grant", ErrorCode.REDIRECT_URI_MISMATCH.getCode()
+            );
         }
 
         String plainTextToken = randomString.run();
