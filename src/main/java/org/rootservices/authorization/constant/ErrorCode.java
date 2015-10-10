505a55ecd34fc67b79bbb5cda6c55053a98a9001
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2015-10-10 09:33:08 -0500

    pt-103900240: resoved compilation issues, must have forgotten to commit these changes.

--- src/main/java/org/rootservices/authorization/constant/ErrorCode.java
+++ src/main/java/org/rootservices/authorization/constant/ErrorCode.java
@@ -23,33 +23,35 @@ public enum ErrorCode {
     REDIRECT_URI_EMPTY_VALUE (11, "redirect uris first item is a empty value."),
     REDIRECT_URI_MORE_THAN_ONE_ITEM (12, "redirect uris has more than one value."),
     REDIRECT_URI_DATA_TYPE (13, "redirect uris first item is not a URI"),
+    REDIRECT_URI_EMPTY_LIST (14, "redirect uris is a empty list"),
+    REDIRECT_URI_NULL (15, "redirect uris is null"),
 
     // client or resource owner -- needs feedback from db.
-    SCOPES_EMPTY_VALUE (14, "scopes first item is a empty value."),
-    SCOPES_MORE_THAN_ONE_ITEM (15, "scopes has more than one value."),
-    SCOPES_DATA_TYPE (16, "scopes first item is not a supported scope"),
-    SCOPES_NOT_SUPPORTED (17, "scope is not available for this client"),
+    SCOPES_EMPTY_VALUE (16, "scopes first item is a empty value."),
+    SCOPES_MORE_THAN_ONE_ITEM (17, "scopes has more than one value."),
+    SCOPES_DATA_TYPE (18, "scopes first item is not a supported scope"),
+    SCOPES_NOT_SUPPORTED (19, "scope is not available for this client"),
 
-    STATE_EMPTY_VALUE (18, "states first item is a empty value"),
-    STATE_MORE_THAN_ONE_ITEM (19, "states has more than one value."),
+    STATE_EMPTY_VALUE (20, "states first item is a empty value"),
+    STATE_MORE_THAN_ONE_ITEM (21, "states has more than one value."),
 
-    CLIENT_NOT_FOUND (20, "client was not found"),
-    RESPONSE_TYPE_MISMATCH (21, "response type provided does not match client's response type."),
-    REDIRECT_URI_MISMATCH (22, "redirect uri provided does not match client's."),
-    RESPONSE_TYPE_NOT_CODE (23, "response type provided is not CODE"),
+    CLIENT_NOT_FOUND (22, "client was not found"),
+    RESPONSE_TYPE_MISMATCH (23, "response type provided does not match client's response type."),
+    REDIRECT_URI_MISMATCH (24, "redirect uri provided does not match client's."),
+    RESPONSE_TYPE_NOT_CODE (25, "response type provided is not CODE"),
 
-    RESOURCE_OWNER_NOT_FOUND (24, "The resource owner was not found"),
-    PASSWORD_MISMATCH (25, "Password did not match"),
-    UNSUPPORTED_ENCODING (26, ""),
+    RESOURCE_OWNER_NOT_FOUND (26, "The resource owner was not found"),
+    PASSWORD_MISMATCH (27, "Password did not match"),
+    UNSUPPORTED_ENCODING (28, ""),
 
-    AUTH_CODE_NOT_FOUND(27, "authorization code was not found"),
-    DUPLICATE_KEY (28, "there was a duplicate key in the payload"),
-    INVALID_PAYLOAD (29, "the payload couldn't be parsed"),
-    MISSING_KEY (30, "A key is missing from the payload"),
-    REDIRECT_URI_INVALID (31, "redirect uri is invalid"),
-    GRANT_TYPE_INVALID (32, "grant type is invalid"),
-    UNKNOWN_KEY (33, "A key is unrecognized"),
-    COMPROMISED_AUTH_CODE (34, "Authorization code has already been used");
+    AUTH_CODE_NOT_FOUND(29, "authorization code was not found"),
+    DUPLICATE_KEY (30, "there was a duplicate key in the payload"),
+    INVALID_PAYLOAD (31, "the payload couldn't be parsed"),
+    MISSING_KEY (32, "A key is missing from the payload"),
+    REDIRECT_URI_INVALID (33, "redirect uri is invalid"),
+    GRANT_TYPE_INVALID (34, "grant type is invalid"),
+    UNKNOWN_KEY (35, "A key is unrecognized"),
+    COMPROMISED_AUTH_CODE (36, "Authorization code has already been used");
 
     private int code;
     private final String message;
