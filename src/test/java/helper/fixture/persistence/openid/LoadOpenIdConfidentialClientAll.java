cec5b020715bb625f6d72c699bb367eb4965d15e
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2016-01-24 10:30:41 -0600

    pt-106943344: mapper to get ro by access token

---
+++ src/test/java/helper/fixture/persistence/openid/LoadOpenIdConfidentialClientAll.java
@@ -0,0 +1,48 @@
+package helper.fixture.persistence.openid;
+
+import helper.fixture.FixtureFactory;
+import helper.fixture.persistence.LoadClientWithScopes;
+import helper.fixture.persistence.LoadConfidentialClientTokenReady;
+import org.rootservices.authorization.persistence.entity.AuthCode;
+import org.rootservices.authorization.persistence.entity.Token;
+import org.rootservices.authorization.persistence.exceptions.DuplicateRecordException;
+import org.rootservices.authorization.persistence.repository.*;
+import org.rootservices.authorization.security.RandomString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.net.URISyntaxException;
+
+/**
+ * Created by tommackenzie on 1/24/16.
+ *
+ * Loads all data associated with a confidential client.
+ *  - scopes (openid)
+ *  - resource owner
+ *  - access request
+ *  - access request scopes (openid)
+ *  - auth code
+ *  - token
+ */
+@Component
+public class LoadOpenIdConfidentialClientAll {
+    private LoadConfidentialClientTokenReady loadConfidentialClientOpendIdTokenReady;
+    private RandomString randomString;
+    private TokenRepository tokenRepository;
+
+    @Autowired
+    public LoadOpenIdConfidentialClientAll(LoadConfidentialClientTokenReady loadConfidentialClientOpendIdTokenReady, RandomString randomString, TokenRepository tokenRepository){
+        this.loadConfidentialClientOpendIdTokenReady = loadConfidentialClientOpendIdTokenReady;
+        this.randomString = randomString;
+        this.tokenRepository = tokenRepository;
+    }
+
+    public Token run() throws DuplicateRecordException, URISyntaxException {
+        AuthCode authCode = loadConfidentialClientOpendIdTokenReady.run(true, false, "plain-text-auth-code");
+        Token token = FixtureFactory.makeToken(authCode.getUuid());
+        String accessToken = randomString.run();
+        token.setToken(accessToken.getBytes());
+        tokenRepository.insert(token);
+        return token;
+    }
+}
--- src/test/java/integration/code/ValidateParams/BaseTest.java
+++ src/test/java/integration/code/ValidateParams/BaseTest.java
@@ -14,6 +14,7 @@ import org.rootservices.authorization.persistence.repository.ScopeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.net.URI;
 
