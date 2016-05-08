631e7032b9a19e4bc3fc70e272b9a6d343ef704c
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2016-05-08 08:09:46 -0500

    pt-119133523: repackaged integration tests.

---
+++ src/test/java/integration/authorization/authenticate/LoginResourceOwnerImplTest.java
@@ -0,0 +1,80 @@
+package integration.authorization.authenticate;
+
+import helper.fixture.FixtureFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.rootservices.authorization.authenticate.LoginResourceOwner;
+import org.rootservices.authorization.authenticate.exception.UnauthorizedException;
+import org.rootservices.authorization.constant.ErrorCode;
+import org.rootservices.authorization.persistence.entity.ResourceOwner;
+import org.rootservices.authorization.persistence.exceptions.RecordNotFoundException;
+import org.rootservices.authorization.persistence.repository.ResourceOwnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * Created by tommackenzie on 4/13/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-auth-test.xml")
+@Transactional
+public class LoginResourceOwnerImplTest {
+
+    @Autowired
+    private ResourceOwnerRepository resourceOwnerRepository;
+    @Autowired
+    private LoginResourceOwner subject;
+
+    @Test
+    public void run() throws UnauthorizedException {
+        ResourceOwner ro = FixtureFactory.makeResourceOwner();
+        resourceOwnerRepository.insert(ro);
+
+        ResourceOwner actual = subject.run(
+            ro.getEmail(), "password"
+        );
+
+        assertThat(actual.getUuid()).isEqualTo(ro.getUuid());
+    }
+
+    @Test
+    public void resourceOwnerNotFound() {
+
+        ResourceOwner actual = null;
+        try {
+            actual = subject.run(
+                "test-" + UUID.randomUUID().toString() + "@rootservices.org", "password"
+            );
+        } catch (UnauthorizedException e) {
+            assertThat(e.getDomainCause()).isInstanceOf(RecordNotFoundException.class);
+            assertThat(e.getCode()).isEqualTo(ErrorCode.RESOURCE_OWNER_NOT_FOUND.getCode());
+        }
+
+        assertThat(actual).isNull();
+    }
+
+    @Test
+    public void passwordIncorrect() {
+        ResourceOwner ro = FixtureFactory.makeResourceOwner();
+        resourceOwnerRepository.insert(ro);
+
+        ResourceOwner actual = null;
+        try {
+            actual = subject.run(
+                 ro.getEmail(), "wrong-password"
+            );
+        } catch (UnauthorizedException e) {
+            assertThat(e.getDomainCause()).isNull();
+            assertThat(e.getCode()).isEqualTo(ErrorCode.PASSWORD_MISMATCH.getCode());
+        }
+
+        assertThat(actual).isNull();
+    }
+
+}
---
+++ src/test/java/integration/authorization/openid/grant/code/request/ValidateOpenIdParams/validation/State/ClientFoundTest.java
@@ -0,0 +1,52 @@
+package integration.authorization.openid.grant.code.request.ValidateOpenIdParams.validation.State;
+
+import helper.ValidateParamsAttributes;
+import integration.authorization.openid.grant.code.request.ValidateOpenIdParams.BaseTest;
+import org.junit.Test;
+import org.rootservices.authorization.constant.ErrorCode;
+import org.rootservices.authorization.oauth2.grant.code.authorization.request.buider.exception.StateException;
+import org.rootservices.authorization.persistence.entity.Client;
+import org.rootservices.authorization.persistence.entity.ResponseType;
+
+
+public class ClientFoundTest extends BaseTest {
+
+    public ValidateParamsAttributes makeValidateParamsAttributes(Client c) {
+        ValidateParamsAttributes p = new ValidateParamsAttributes();
+        p.clientIds.add(c.getUuid().toString());
+        p.redirectUris.add(c.getRedirectURI().toString());
+        p.responseTypes.add(ResponseType.CODE.toString());
+
+        return p;
+    }
+
+    @Test
+    public void stateHasTwoItemsShouldThrowInformClientException() throws Exception {
+        Client c = loadClientWithOpenIdScope.run();
+
+        ValidateParamsAttributes p = makeValidateParamsAttributes(c);
+        p.states.add("some-state");
+        p.states.add("some-state");
+
+        Exception expectedDomainCause = new StateException();
+        int expectedErrorCode = ErrorCode.STATE_MORE_THAN_ONE_ITEM.getCode();
+        String expectedError = "invalid_request";
+
+        runExpectInformClientException(p, expectedDomainCause, expectedErrorCode, expectedError, c.getRedirectURI());
+    }
+
+    @Test
+    public void stateIsBlankStringShouldThrowInformClientException() throws Exception {
+        Client c = loadClientWithOpenIdScope.run();
+
+        ValidateParamsAttributes p = makeValidateParamsAttributes(c);
+        p.states.add("");
+
+        Exception expectedDomainCause = new StateException();
+        int expectedErrorCode = ErrorCode.STATE_EMPTY_VALUE.getCode();
+        String expectedError = "invalid_request";
+
+        runExpectInformClientException(p, expectedDomainCause, expectedErrorCode, expectedError, c.getRedirectURI());
+
+    }
+}
