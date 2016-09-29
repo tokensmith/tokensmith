07f368912781a82d3fa6d2474fc7e6dc3dcb1476
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2016-09-28 21:13:18 -0500

    pt-116818393: added requestTokenCodeGrant to factory

--- src/test/java/org/rootservices/authorization/oauth2/grant/token/RequestTokenGrantFactoryTest.java
+++ src/test/java/org/rootservices/authorization/oauth2/grant/token/RequestTokenGrantFactoryTest.java
@@ -4,7 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.rootservices.authorization.oauth2.grant.token.factory.RequestTokenFactory;
+import org.rootservices.authorization.oauth2.grant.redirect.code.token.RequestTokenCodeGrant;
+import org.rootservices.authorization.oauth2.grant.token.factory.RequestTokenGrantFactory;
 import org.rootservices.authorization.oauth2.grant.password.RequestTokenPasswordGrant;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -15,22 +16,30 @@ import static org.junit.Assert.*;
 /**
  * Created by tommackenzie on 9/21/16.
  */
-public class RequestTokenFactoryTest {
+public class RequestTokenGrantFactoryTest {
     @Mock
     private RequestTokenPasswordGrant mockRequestTokenPasswordGrant;
+    @Mock
+    private RequestTokenCodeGrant mockRequestTokenCodeGrant;
 
-    private RequestTokenFactory subject;
+    private RequestTokenGrantFactory subject;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        subject = new RequestTokenFactory(mockRequestTokenPasswordGrant);
+        subject = new RequestTokenGrantFactory(mockRequestTokenPasswordGrant, mockRequestTokenCodeGrant);
     }
 
     @Test
     public void makeShouldBePassword() throws Exception {
         RequestTokenGrant actual = subject.make("password");
         assertThat(actual, instanceOf(RequestTokenPasswordGrant.class));
+    }
+
+    @Test
+    public void makeShouldBeCode() throws Exception {
+        RequestTokenGrant actual = subject.make("code");
+        assertThat(actual, instanceOf(RequestTokenCodeGrant.class));
     }
 
     @Test
