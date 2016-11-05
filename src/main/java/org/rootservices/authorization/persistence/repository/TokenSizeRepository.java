df8bd4ed22dfb0f9766011dc7fab074720f9571b
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2016-11-05 08:01:17 -0500

    131510989: persistence for token size

---
+++ src/main/java/org/rootservices/authorization/persistence/repository/TokenSizeRepository.java
@@ -0,0 +1,10 @@
+package org.rootservices.authorization.persistence.repository;
+
+import org.rootservices.authorization.persistence.entity.TokenSize;
+
+/**
+ * Created by tommackenzie on 11/5/16.
+ */
+public interface TokenSizeRepository {
+    TokenSize get();
+}
---
+++ src/main/resources/db/migration/V1_20161105071600__seed_token_size.sql
@@ -0,0 +1,1 @@
+insert into token_size values ('6898348b-1423-4e00-ab8c-17b13e61af1c', 32, 32, 32);
