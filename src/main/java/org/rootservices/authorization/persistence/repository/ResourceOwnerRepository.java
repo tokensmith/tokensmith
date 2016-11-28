f251e51f6d2b25fe72d95a0e4e9f694eb272f4e7
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2016-11-27 18:21:39 -0600

    134917961: added profile and tokens to resource owner

--- src/main/java/org/rootservices/authorization/persistence/repository/ResourceOwnerRepository.java
+++ src/main/java/org/rootservices/authorization/persistence/repository/ResourceOwnerRepository.java
@@ -12,5 +12,6 @@ public interface ResourceOwnerRepository {
     ResourceOwner getById(UUID id) throws RecordNotFoundException;
     ResourceOwner getByEmail(String email) throws RecordNotFoundException;
     ResourceOwner getByAccessToken(String accessToken) throws RecordNotFoundException;
+    ResourceOwner getByAccessTokenWithProfileAndTokens(String accessToken) throws RecordNotFoundException;
     void insert(ResourceOwner resourceOwner);
 }
