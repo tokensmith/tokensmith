8187787a7b27a5001bb4cf6b5e9df3fb111e057d
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2016-11-27 18:50:54 -0600

    134917961: integrated getByAccessTokenWithProfileAndTokens

--- src/main/java/org/rootservices/authorization/openId/identity/exception/ProfileNotFoundException.java
+++ src/main/java/org/rootservices/authorization/openId/identity/exception/ProfileNotFoundException.java
@@ -4,6 +4,10 @@ package org.rootservices.authorization.openId.identity.exception;
  * Created by tommackenzie on 3/27/16.
  */
 public class ProfileNotFoundException extends Exception {
+    public ProfileNotFoundException(String message) {
+        super(message);
+    }
+
     public ProfileNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
---
+++ src/main/java/org/rootservices/authorization/openId/identity/exception/ResourceOwnerNotFoundException.java
@@ -0,0 +1,10 @@
+package org.rootservices.authorization.openId.identity.exception;
+
+/**
+ * Created by tommackenzie on 11/27/16.
+ */
+public class ResourceOwnerNotFoundException extends Exception {
+    public ResourceOwnerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
