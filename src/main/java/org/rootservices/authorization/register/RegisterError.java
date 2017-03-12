9b5ce4e38b78d13b6d8e52d96763bf2312eee408
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2017-03-12 09:58:30 -0500

    94748146: renamed MakeResourceOwner to Register

---
+++ src/main/java/org/rootservices/authorization/register/RegisterError.java
@@ -0,0 +1,10 @@
+package org.rootservices.authorization.register;
+
+/**
+ * Created by tommackenzie on 3/12/17.
+ */
+public enum RegisterError {
+    UNKNOWN,
+    EMAIL_TAKEN,
+    PASSWORD_MISMATCH
+}
