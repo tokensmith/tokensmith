a49dffbe084fde17e67a5d45ea25d9356476b3ff
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2017-03-12 17:21:28 -0500

    94748146: validate input fields are not blank or null

--- src/main/java/org/rootservices/authorization/register/RegisterError.java
+++ src/main/java/org/rootservices/authorization/register/RegisterError.java
@@ -4,6 +4,9 @@ package org.rootservices.authorization.register;
  * Created by tommackenzie on 3/12/17.
  */
 public enum RegisterError {
+    EMAIL_MISSING,
+    PASSWORD_MISSING,
+    REPEAT_PASSWORD_MISSING,
     UNKNOWN,
     EMAIL_TAKEN,
     PASSWORD_MISMATCH
