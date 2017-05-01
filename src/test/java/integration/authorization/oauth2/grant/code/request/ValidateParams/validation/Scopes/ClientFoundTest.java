dab2646a00dd8415db6f92f92a0185017f8d5915
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2017-05-01 10:01:42 -0500

    oauth-parse: integrated parser to oauth

--- src/test/java/integration/authorization/oauth2/grant/code/request/ValidateParams/validation/Scopes/ClientFoundTest.java
+++ src/test/java/integration/authorization/oauth2/grant/code/request/ValidateParams/validation/Scopes/ClientFoundTest.java
@@ -1,32 +1,39 @@
 package integration.authorization.oauth2.grant.code.request.ValidateParams.validation.Scopes;
 
-import helper.ValidateParamsAttributes;
+
 import integration.authorization.oauth2.grant.code.request.ValidateParams.BaseTest;
 import org.junit.Test;
 import org.rootservices.authorization.constant.ErrorCode;
-import org.rootservices.authorization.oauth2.grant.redirect.shared.authorization.request.factory.exception.ScopesException;
 import org.rootservices.authorization.oauth2.grant.redirect.shared.authorization.request.factory.exception.StateException;
+import org.rootservices.authorization.parse.exception.OptionalException;
 import org.rootservices.authorization.persistence.entity.Client;
 import org.rootservices.authorization.persistence.entity.ResponseType;
 
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
 
 
 public class ClientFoundTest extends BaseTest {
+
+    public Map<String, List<String>> makeParams(Client c) {
+        Map<String, List<String>> p = super.makeParams();
+        p.get("client_id").add(c.getId().toString());
+
+        for(ResponseType rt: c.getResponseTypes()) {
+            p.get("response_type").add(rt.getName());
+        }
+        p.get("state").add("some-state");
+
+        return p;
+    }
 
     @Test
     public void scopeIsInvalidShouldThrowInformClientException() throws URISyntaxException, StateException {
         Client c = loadConfidentialClient();
 
-        ValidateParamsAttributes p = new ValidateParamsAttributes();
-        p.clientIds.add(c.getId().toString());
-        p.states.add("some-state");
-
-        for(ResponseType rt: c.getResponseTypes()) {
-            p.responseTypes.add(rt.getName());
-        }
-
-        p.scopes.add("invalid-scope");
+        Map<String, List<String>> p = makeParams(c);
+        p.get("scope").add("invalid-scope");
 
         int expectedErrorCode = ErrorCode.SCOPES_NOT_SUPPORTED.getCode();
         String expectedDescription = ErrorCode.SCOPES_NOT_SUPPORTED.getDescription();
@@ -39,44 +46,28 @@ public class ClientFoundTest extends BaseTest {
     public void scopesHasTwoItemsShouldThrowInformClientException() throws URISyntaxException, StateException {
         Client c = loadConfidentialClient();
 
-        ValidateParamsAttributes p = new ValidateParamsAttributes();
-        p.clientIds.add(c.getId().toString());
-        p.states.add("some-state");
+        Map<String, List<String>> p = makeParams(c);
+        p.get("scope").add("profile");
+        p.get("scope").add("profile");
 
-        for(ResponseType rt: c.getResponseTypes()) {
-            p.responseTypes.add(rt.getName());
-        }
-
-        p.scopes.add("profile");
-        p.scopes.add("profile");
-
-        Exception expectedDomainCause = new ScopesException();
-        int expectedErrorCode = ErrorCode.SCOPES_MORE_THAN_ONE_ITEM.getCode();
+        Exception cause = new OptionalException();
         String expectedDescription = ErrorCode.SCOPES_MORE_THAN_ONE_ITEM.getDescription();
         String expectedError = "invalid_request";
 
-        runExpectInformClientExceptionWithState(p, expectedDomainCause, expectedErrorCode, expectedError, expectedDescription, c.getRedirectURI());
+        runExpectInformClientExceptionWithState(p, cause, 1, expectedError, expectedDescription, c.getRedirectURI());
     }
 
     @Test
     public void scopeIsBlankStringShouldThrowInformClientException() throws URISyntaxException, StateException {
         Client c = loadConfidentialClient();
 
-        ValidateParamsAttributes p = new ValidateParamsAttributes();
-        p.clientIds.add(c.getId().toString());
-        p.states.add("some-state");
+        Map<String, List<String>> p = makeParams(c);
+        p.get("scope").add("");
 
-        for(ResponseType rt: c.getResponseTypes()) {
-            p.responseTypes.add(rt.getName());
-        }
-
-        p.scopes.add("");
-
-        Exception expectedDomainCause = new ScopesException();
-        int expectedErrorCode = ErrorCode.SCOPES_EMPTY_VALUE.getCode();
+        Exception cause = new OptionalException();
         String expectedDescription = ErrorCode.SCOPES_EMPTY_VALUE.getDescription();
         String expectedError = "invalid_scope";
 
-        runExpectInformClientExceptionWithState(p, expectedDomainCause, expectedErrorCode, expectedError, expectedDescription, c.getRedirectURI());
+        runExpectInformClientExceptionWithState(p, cause, 1, expectedError, expectedDescription, c.getRedirectURI());
     }
 }
