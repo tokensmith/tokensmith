f65455b151ea04dadfd56e8516888b8dbd7a9fbb
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2017-10-30 17:42:00 -0500

    141491921: revokes active refresh tokens.

--- src/main/java/org/rootservices/authorization/persistence/mapper/ResourceOwnerTokenMapper.java
+++ src/main/java/org/rootservices/authorization/persistence/mapper/ResourceOwnerTokenMapper.java
@@ -5,9 +5,7 @@ import org.rootservices.authorization.persistence.entity.ResourceOwnerToken;
 
 import java.util.UUID;
 
-/**
- * Created by tommackenzie on 4/19/16.
- */
+
 public interface ResourceOwnerTokenMapper {
     void insert(@Param("resourceOwnerToken") ResourceOwnerToken resourceOwnerToken);
     ResourceOwnerToken getByAccessToken(@Param("accessToken") String accessToken);
