9a1154b261e2d870439381e2835048b7bf56970d
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2017-10-30 17:42:00 -0500

    141491921: revokes active refresh tokens.

--- src/main/java/org/rootservices/authorization/persistence/mapper/RefreshTokenMapper.java
+++ src/main/java/org/rootservices/authorization/persistence/mapper/RefreshTokenMapper.java
@@ -6,16 +6,18 @@ import org.rootservices.authorization.persistence.entity.RefreshToken;
 import org.rootservices.authorization.persistence.exceptions.RecordNotFoundException;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
-/**
- * Created by tommackenzie on 10/2/16.
- */
+
 @Repository
 public interface RefreshTokenMapper {
     void insert(@Param("refreshToken") RefreshToken refreshToken);
     RefreshToken getByTokenId(@Param("tokenId") UUID tokenId);
     RefreshToken getByClientIdAndAccessToken(@Param("clientId") UUID clientId, @Param("accessToken") String accessToken);
+    List<RefreshToken> getByResourceOwner(@Param("resourceOwnerId") UUID resourceOwnerId);
     void revokeByAuthCodeId(@Param("authCodeId") UUID authCodeId);
     void revokeByTokenId(@Param("tokenId") UUID tokenId);
+    void revokeActive(@Param("resourceOwnerId") UUID resourceOwnerId);
+
 }
